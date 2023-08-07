225b905add7e03ecf7d2ec9e1d82a8e0ceed693c
Mridul Tripathi <98095736+mridul588@users.noreply.github.com>
2023-08-08 00:26:12 +0530

    Update index.js

--- Registration-form/server/index.js
+++ Registration-form/server/index.js
@@ -4,6 +4,12 @@ const dotenv = require("dotenv").config();
 const {mongoose} = require("mongoose");
 const cookieParser = require("cookie-parser");
 
+
+const MONGO_URL = "mongodb+srv://b121030:" +
+  encodeURIComponent("KeSDpjGp0DpyliZr") +
+  "@cluster0.6wx6mzn.mongodb.net/test?retryWrites=true&w=majority";
+
+
 mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("db connected");
 }).catch((e) => {console.log(e);})
